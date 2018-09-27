0335a1dd54fa80da6880fb22e3613c93c731fe16
Matthew Kim <matthew.kim@aa.com>
2018-09-27 13:58:06 -0500

    implement app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(_name_)
+
+@app.route("/")
+def main():
+  return "Welcome!"
+
+app.route ('/how are you')
+def hello():
+  return 'I am good, how are you?'
+
+  if _name_ == "_main_":
+    app.run()
